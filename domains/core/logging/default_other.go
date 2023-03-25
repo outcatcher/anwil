6cd2e5f00be956b16c730dc0f06da03513323a07
Anton Kachurin <katchuring@gmail.com>
2023-03-25 20:47:53 +0300

    Fix `GetDefaultLogger
    Make `GetDefaultLogger` correctly default to `log.Default()`
    in case syslog is not available (e.g. running in docker)

--- domains/core/logging/default_other.go
+++ domains/core/logging/default_other.go
@@ -5,21 +5,13 @@ package logging
 import (
 	"log"
 	"log/syslog"
-	"os"
 )
-
-// syslog can be unavailable, e.g. in docker.
-const syslogEnvVar = "SYSLOG"
 
 // GetDefaultLogger returns syslogger with "anwil-migrate" prefix.
 func GetDefaultLogger() *log.Logger {
-	if os.Getenv(syslogEnvVar) == "" {
-		return log.Default()
-	}
-
 	sysWriter, err := syslog.New(syslog.LOG_INFO, "anwil-api")
 	if err != nil {
-		log.Fatalln(err)
+		return log.Default()
 	}
 
 	sysLogger := log.New(sysWriter, "", log.LstdFlags)
